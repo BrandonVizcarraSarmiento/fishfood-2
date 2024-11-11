2ae6bd8eab7ec02d2853ccadf3dd38e38ff493a9
Brandon <brandon.vizcarra@sfact.pe>
2024-11-11 17:28:52 -0500

    boton contactanos

--- frontend/app/(routes)/(home)/productos/components/infoProducto.tsx
+++ frontend/app/(routes)/(home)/productos/components/infoProducto.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { useGetRedes } from "@/api/redes/getRedes";
 import { Separator } from "@/components/ui/separator";
 
 interface InfoProductoProps {
@@ -8,30 +8,42 @@ interface InfoProductoProps {
 }
 
 const InfoProducto: React.FC<InfoProductoProps> = ({ nombre, precio, descripcion }) => {
+    const { redes, loading, error } = useGetRedes();
+
+    const enlaceRedSocial = redes.find((red) => red.id === 1)?.linkRedes;
+
+    if (loading) return <p>Cargando redes sociales...</p>;
+    if (error) return <p>Error: {error}</p>;
+
     return (
         <div className="space-y-4 p-4">
-            {/* Título del producto centrado y más grande */}
             <div className="text-center space-y-4">
                 <h2 className="text-5xl font-bold">{nombre}</h2>
             </div>
 
-            {/* Precio alineado a la izquierda y más grande */}
             <div className="space-y-2">
                 <p className="px-4 py-2 text-lg text-white font-bold bg-green-500 rounded-full w-fit">S/{precio}</p>
             </div>
 
             <Separator />
-
-            {/* Subtítulo de descripción y contenido */}
             <div>
                 <h3 className="text-xl font-semibold pb-2">Descripción: </h3>
                 <p>{descripcion}</p>
             </div>
+            <Separator />
 
-            {/* Botón de contacto */}
-            <div className="flex justify-center">
-                <Button>Contáctanos</Button>
-            </div>
+            {enlaceRedSocial && (
+                <div className="flex justify-center mt-4">
+                    <a
+                        href={enlaceRedSocial}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="px-6 py-3 bg-blue-500 text-white rounded-md text-lg font-semibold hover:bg-blue-600"
+                    >
+                        Contáctanos
+                    </a>
+                </div>
+            )}
         </div>
     );
 };
